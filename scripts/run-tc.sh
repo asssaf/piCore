de768edd8e0848c905d86c993e2fbc47aa2ed562
assaf <445552+asssaf@users.noreply.github.com>
2026-06-17 17:54:46 -0700

    force cortex-a72 in github actions to avoid FEAT_E0PD error: ERROR:target/arm/internals.h:767:regime_is_user: code should not be reached

--- scripts/run-tc.sh
+++ scripts/run-tc.sh
@@ -3,18 +3,21 @@
 set -euxo pipefail
 
 : ${BASE:=./}
-: ${KERNEL_VERSION:=6.12.25}
+: ${KERNEL_VERSION:=6.18.28}
 : ${PACKAGE:=piCore64}
-: ${PICORE_VERSION_MAJOR:=16}
+: ${PICORE_VERSION_MAJOR:=17}
 : ${PICORE_VERSION_MINOR=0}
 : ${PICORE_VERSION_MICRO=0}
-: ${ROOTFS_SUFFIX:=gz}
+: ${ROOTFS_SUFFIX:=zst}
+: ${CPU:=max}
+: ${SMP:=4}
+: ${MEM:=2G}
 
 KERNEL="${BASE}/Image-${KERNEL_VERSION}"
 INITRD="${BASE}/rootfs-${PACKAGE}-${PICORE_VERSION_MAJOR}.${PICORE_VERSION_MINOR}.${ROOTFS_SUFFIX}"
 IMAGE="${BASE}/firmware-with-tce-disk-${PACKAGE}.img"
 CMDLINE="console=ttyAMA0,115200 console=ttyGS0,115200 text root=/dev/ram0 nortc rootwait loglevel=3 tce=LABEL=RECOVERY tz=PST+8PDT,M3.2.0/2,M11.1.0/2 debug=y nozswap"
-qemu-system-aarch64 -machine virt -cpu max -smp 4 -m 2G \
+qemu-system-aarch64 -machine virt -cpu ${CPU} -smp ${SMP} -m ${MEM} \
 	-kernel "$KERNEL" -append "$CMDLINE" \
 	-initrd "$INITRD" \
 	-nographic \
